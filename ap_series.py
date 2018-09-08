340992064cdca3f8affe50515f696f09c4244536
DESKTOP-NDJONLN\Ajay <ajaybharti979@gmail.com>
2018-09-08 17:26:50 +0530

    Ap Series progrm

---
+++ ap_series.py
@@ -0,0 +1,6 @@
+# Find the series by entering first term, last term and common difference
+f = int(input("Enter the first term : "))
+l = int (input("Enter the last term : "))
+cd = int (input("Enter the Common Difference : "))
+l = l + 1;
+print(range(f, l, cd))
