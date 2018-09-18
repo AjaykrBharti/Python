bc29c25ff1e2ee16f50a47ac5091ba4acafa7189
Ajay Kumar Bharti <ajay.ajaykumar.kumar494@gmail.com>
2018-09-18 16:33:32 +0530

    Add files via upload

---
+++ first_turtle.py
@@ -0,0 +1,54 @@
+import turtle
+windowScree = turtle.Screen()
+tName = turtle.Turtle()
+
+tName.left(60)
+tName.forward(80)
+tName.right(120)
+tName.forward(80)
+tName.right(180)
+tName.forward(40)
+tName.left(90)
+tName.forward(70)
+tName.left(180)
+tName.forward(120)
+tName.right(120)
+tName.forward(80)
+for i in range(0,30,1):
+    tName.right(i)
+    tName.forward(3)
+tName.right(70)   
+tName.right(70)
+tName.forward(100)
+tName.right(40)
+tName.forward(40)
+tName.right(110)
+tName.forward(60)
+tName.right(180)
+tName.forward(30)
+tName.left(90)
+for i in range(0,20,1):
+    tName.left(i)
+    tName.forward(4)
+
+tName.forward(50)
+for i in range(0,30,3):
+    tName.left(i)
+    tName.forward(4)
+
+tName.forward(30)
+tName.right(180)
+tName.forward(38)
+tName.left(90)
+tName.forward(40)
+for i in range(0,40,4):
+    tName.left(i)
+    tName.forward(2)
+tName.left(10)
+tName.forward(150)
+
+
+
+
+
+
